2234a0efe0dc8d2420b0bd619a3d2f174a7be5eb
eduardonk9999 <thiago.novak7@gmail.com>
2022-05-25 11:55:53 -0300

    aulas inciais de python

---
+++ Aula2/app.py
@@ -0,0 +1,10 @@
+nome = "Eduardo"
+idade = "30"
+gosto_de = "Pizza 🍕"
+
+print(f"Meu nome é {nome}. Tenho {idade} anos e gosto de {gosto_de}")
+
+# Quatro tipo de dados
+# String, Number (Int, float), Bolean (true || false)
+
+
